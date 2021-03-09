a69f760a39cd9d77628026860a4952c535851e2f
Osvaldinei Oliveira <valdinei.olv@gmail.com>
2021-03-08 21:37:58 -0300

    Splash Screen

--- flare_test/lib/main.dart
+++ flare_test/lib/main.dart
@@ -13,19 +13,19 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Home(),
+      home: Splash(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class Home extends StatefulWidget {
-  const Home({Key key}) : super(key: key);
+class Splash extends StatefulWidget {
+  const Splash({Key key}) : super(key: key);
   @override
-  _HomeState createState() => _HomeState();
+  _SplashState createState() => _SplashState();
 }
 
-class _HomeState extends State<Home> {
+class _SplashState extends State<Splash> {
   void _togglePlay() {
     setState(() => _controller.isActive = !_controller.isActive);
   }
@@ -51,21 +51,17 @@ class _HomeState extends State<Home> {
         }
       },
     );
+
+    Future.delayed(Duration(seconds: 4)).then((_) {
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (context) => Home()));
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: GestureDetector(
-        onTap: () {
-          setState(() {
-            if (_anim == "spin1")
-              _anim = "spin2";
-            else
-              _anim = "spin2";
-          });
-        },
         child: Container(
           width: 200,
           height: 200,
@@ -73,14 +69,76 @@ class _HomeState extends State<Home> {
               ? const SizedBox()
               : Rive(artboard: _riveArtboard),
         ),
-      )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _togglePlay,
-        tooltip: isPlaying ? 'Pause' : 'Play',
-        child: Icon(
-          isPlaying ? Icons.pause : Icons.play_arrow,
-        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _togglePlay,
+      //   tooltip: isPlaying ? 'Pause' : 'Play',
+      //   child: Icon(
+      //     isPlaying ? Icons.pause : Icons.play_arrow,
+      //   ),
+      // ),
+    );
+  }
+}
+
+
+// #################################################################################
+
+class Home extends StatefulWidget {
+  const Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final riveFileName = 'assets/animheart.riv';
+  Artboard _artboard;
+
+  @override
+  void initState() {
+    _loadRiveFile();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text(
+            "Flutter + Rive",
+            style: TextStyle(fontSize: 40),
+            textAlign: TextAlign.center,
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            child: _artboard != null
+                ? Rive(
+                    artboard: _artboard,
+                    fit: BoxFit.contain,
+                  )
+                : Container(),
+          )
+        ],
       ),
     );
+  }
+
+  // loads a Rive file
+  void _loadRiveFile() async {
+    final bytes = await rootBundle.load(riveFileName);
+    final file = RiveFile();
+
+    if (file.import(bytes)) {
+      // Select an animation by its name
+      setState(() => _artboard = file.mainArtboard
+        ..addController(
+          SimpleAnimation('pulse'),
+        ));
+    }
   }
 }
